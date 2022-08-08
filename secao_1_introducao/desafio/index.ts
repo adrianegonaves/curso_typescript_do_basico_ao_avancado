548912fb0e52ce2abc32219866c0eea47dc81a26
Perkles <adrianegonaves@gmail.com>
2022-08-08 18:28:46 -0300

    Introdução a TS

---
+++ secao_1_introducao/desafio/index.ts
@@ -0,0 +1,4 @@
+function fazSoma (numero1: number, numero2: number){
+    return numero1 + numero2
+}
+console.log(fazSoma(2,2))
---
+++ secao_1_introducao/index.ts
@@ -0,0 +1,16 @@
+// const firstName = "Adriane"
+// const anotherName = 1
+// const x = true
+
+// // tipando a propriedade 
+// function greeting (name: string){
+//     console.log("Olá "  +  name)
+// }
+// greeting(firstName)
+// // greeting(anotherName);
+// // greeting(x);
+
+function fazSoma (numero1: number, numero2: number){
+    return numero1 + numero2
+}
+console.log(fazSoma(2,2))
